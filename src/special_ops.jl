a56b37f3645853cfd11f86b47cd5976c5e09f610
cmhamel <cmhamel32@gmail.com>
2023-05-04 13:52:33 -0400

    adding dotdot for non symmetric fourth order tensors

--- src/special_ops.jl
+++ src/special_ops.jl
@@ -21,3 +21,26 @@ such that ``a_k C_{ikjl} b_l``.
         @inbounds return Tensor{2, dim}($exps)
     end
 end
+
+"""
+    dotdot(::Vec, ::FourthOrderTensor, ::Vec)
+
+Computes a special dot product between two vectors and a fourth order tensor
+such that ``a_k C_{ikjl} b_l``.
+"""
+@generated function dotdot(v1::Vec{dim}, S::Tensor{4, dim}, v2::Vec{dim}) where {dim}
+    idx(i,j,k,l) = compute_index(Tensor{4, dim}, i, j, k, l)
+    exps = Expr(:tuple)
+    for j in 1:dim, i in 1:dim
+        ex1, ex2 = Expr[],Expr[]
+        for l in 1:dim, k in 1:dim
+            push!(ex1, :(get_data(v1)[$k] * get_data(S)[$(idx(i,k,j,l))]))
+            push!(ex2, :(get_data(v2)[$l]))
+        end
+        push!(exps.args, reducer(ex1, ex2, true))
+    end
+    return quote
+        $(Expr(:meta, :inline))
+        @inbounds return Tensor{2, dim}($exps)
+    end
+end
